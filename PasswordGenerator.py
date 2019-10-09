5cdcd88c177f7b8230d655cc0dcc8e2549033bca
Aliaksei Rak <kventinel@gmail.com>
2019-10-10 00:27:37 +0300

    update Password Generator

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,9 +1,27 @@
-import string
+#!/usr/bin/env python3
+import argparse
 import random
+import string
 
-def pass_generator(size=16, chars=string.ascii_letters + string.digits):
+def _parse_args():
+    parser = argparse.ArgumentParser(description='Script for generate random password')
+    parser.add_argument('--only-digits', action='store_true',
+                        help='use, if want generate password only from digits')
+    parser.add_argument('--size', type=int, default=16, help='size of password')
+    return parser.parse_args()
+
+def pass_generator(size, symbols):
+    """
+    Generate password of given size from given symbols.
+    """
     random.seed();
-    return ''.join(random.choice(chars) for _ in range(size))
+    return ''.join(random.choice(symbols) for _ in range(size))
+
+def _main(args):
+    symbols = string.digits
+    if not args.only_digits:
+        symbols += string.ascii_letters
+    print(pass_generator(args.size, symbols))
 
 if __name__ == "__main__":
-    print(pass_generator())
+    _main(_parse_args())
