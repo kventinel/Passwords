41a66854f727985056cba5ddc7dd10b45903033a
Aliaksei Rak <kventinel@gmail.com>
2022-12-11 18:18:33 +0300

    update password generator

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -5,8 +5,15 @@ import string
 
 def _parse_args():
     parser = argparse.ArgumentParser(description='Script for generate random password')
-    parser.add_argument('--only-digits', action='store_true',
-                        help='use, if want generate password only from digits')
+    symbols_group = parser.add_mutually_exclusive_group()
+    symbols_group.add_argument(
+        '--digits', action='store_true',
+        help='use, if want generate password only from digits'
+    )
+    symbols_group.add_argument(
+        '--digits-and-letters', action='store_true',
+        help='use, if want generate password only from digits and letters'
+    )
     parser.add_argument('--size', type=int, default=16, help='size of password')
     return parser.parse_args()
 
@@ -19,9 +26,10 @@ def pass_generator(size, symbols):
 
 def _main(args):
     symbols = string.digits
-    if not args.only_digits:
+    if not args.digits:
         symbols += string.ascii_letters
-        symbols += '!@#$%^&*()_-=+,<.>/?'
+        if not args.digits_and_letters:
+            symbols += '!@#$%^&*()_-=+,<.>/?'
     print(pass_generator(args.size, symbols))
 
 if __name__ == "__main__":
